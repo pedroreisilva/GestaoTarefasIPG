60ac0de69c41110ef44372561599605fd3f4e0c3
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-12-01 22:39:06 +0000

    Create DepartamentosController

---
+++ GestaoTarefasIPG/Models/Departamentos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Departamentos
+    {
+
+        [Required]
+        [Key]
+        public int idDepartamento { get; set; }
+
+        [Required]
+        public String NomeDepartamento { get; set; }
+
+    }
+}
