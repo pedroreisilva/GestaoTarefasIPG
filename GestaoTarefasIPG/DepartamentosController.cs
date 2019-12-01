ee4c32c5878a2e280becaa828e82bf2f47ed1e00
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-12-01 22:42:00 +0000

    Update DepartamentosController

--- GestaoTarefasIPG/DepartamentosController.cs
+++ GestaoTarefasIPG/DepartamentosController.cs
@@ -2,10 +2,156 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestaoTarefasIPG.Data;
+using GestaoTarefasIPG.Models;
 
 namespace GestaoTarefasIPG
 {
-    public class DepartamentosController
+    public class DepartamentosController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DepartamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departamentos.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamentos = await _context.Departamentos
+                .FirstOrDefaultAsync(m => m.idDepartamento == id);
+            if (departamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("idDepartamento,NomeDepartamento")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamentos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamentos = await _context.Departamentos.FindAsync(id);
+            if (departamentos == null)
+            {
+                return NotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST:Departamentos/Edit
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("idDepartamento,NomeDepartamento")] Departamentos departamento)
+        {
+            if (id != departamento.idDepartamento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.idDepartamento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        private bool DepartamentoExists(int idDepartamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: Departamentos/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamentos
+                .FirstOrDefaultAsync(m => m.idDepartamento == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var departamento = await _context.Departamentos.FindAsync(id);
+            _context.Departamentos.Remove(departamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentosExists(int id)
+        {
+            return _context.Departamentos.Any(e => e.idDepartamento == id);
+        }
     }
 }
