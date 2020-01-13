3fe9bfd84b696645f196d18759c6492b39377ae3
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2020-01-13 12:39:11 +0000

    Revert "Criação do Login no SeedData"
    This reverts commit f26fd14038cd11cd50f38434e4c77872e6a08fba.

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,11 +7,6 @@ namespace GestaoTarefasIPG.Models
 {
     public class SeedData
     {
-
-        private const string ADMIN = "admin";
-        private const string MANAGER = "manager";
-        private const string FUNCIONARIO = "funcionario";
-
         public static void Populate(GestaoTarefasIPGContext db)
         {
             PopulateCargos(db);
@@ -36,97 +30,6 @@ namespace GestaoTarefasIPG.Models
 
 
         }
-
-        public static async Task PopulateUsersAsync(UserManager<IdentityUser> userManager)
-        {
-            const string ADMIN_ID = "admin@ipg.pt";
-            const string ADMIN_PW = "asd123";
-
-            const string MANAGER_ID = " manager@ipg.pt";
-            const string MANAGER_PW = "asd123";
-
-            IdentityUser user = await userManager.FindByNameAsync(ADMIN_ID);
-            if (user == null)
-            {
-                user = new IdentityUser
-                {
-                    UserName = ADMIN_ID,
-                    Email = ADMIN_ID
-                };
-
-                await userManager.CreateAsync(user, ADMIN_PW);
-            }
-
-            if (!await userManager.IsInRoleAsync(user, ADMIN))
-            {
-                await userManager.AddToRoleAsync(user, ADMIN);
-            }
-
-            user = await userManager.FindByNameAsync(MANAGER_ID);
-            if (user == null)
-            {
-                user = new IdentityUser
-                {
-                    UserName = MANAGER_ID,
-                    Email = MANAGER_ID
-                };
-
-                await userManager.CreateAsync(user, MANAGER_PW);
-
-            }
-
-            if (!await userManager.IsInRoleAsync(user, MANAGER))
-            {
-                await userManager.AddToRoleAsync(user, MANAGER);
-            }
-
-            user = await userManager.FindByNameAsync("teste1@ipg.pt");
-            if (user == null)
-            {
-                user = new IdentityUser
-                {
-                    UserName = "teste1@ipg.pt",
-                    Email = "teste1@ipg.pt"
-                };
-                await userManager.CreateAsync(user, ADMIN_PW);
-            }
-
-
-            user = await userManager.FindByNameAsync("teste2@ipg.pt");
-            if (user == null)
-            {
-                user = new IdentityUser
-                {
-                    UserName = "teste2@ipg.pt",
-                    Email = "teste2@ipg.pt"
-                };
-                await userManager.CreateAsync(user, ADMIN_PW);
-
-            }
-        }
-
-        public static async Task CreateRolesAsync(RoleManager<IdentityRole> roleManager)
-        {
-            if(!await roleManager.RoleExistsAsync(ADMIN))
-            {
-                await roleManager.CreateAsync(new IdentityRole(ADMIN));
-            }
-
-            if(!await roleManager.RoleExistsAsync(MANAGER))
-            {
-                await roleManager.CreateAsync(new IdentityRole(MANAGER));
-            }
-
-            if (!await roleManager.RoleExistsAsync(FUNCIONARIO))
-            {
-                await roleManager.CreateAsync(new IdentityRole(MANAGER));
-
-            }
-        }
-
-
-
-        
 
         public static void PopulateDivisao(GestaoTarefasIPGContext db)
         {
